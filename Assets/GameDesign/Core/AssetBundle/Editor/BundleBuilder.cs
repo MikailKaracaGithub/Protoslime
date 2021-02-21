061333c603c397fb883c6f92e79c50d392debe2b
Xavaltir <53319123+Xavaltir@users.noreply.github.com>
2021-02-21 02:52:56 +0100

    Initial Commit

---
+++ Assets/GameDesign/Core/AssetBundle/Editor/BundleBuilder.cs
@@ -0,0 +1,84 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+using System.IO;
+
+using GameDesign.Core.AssetBundle;
+
+public class BundleBuilder : Editor
+{
+    static string _pathAssembly;
+
+    [MenuItem("Game Design/Export/Build AssetBundles")]
+    static void BuildAssetbundles()
+    {
+        if( SetAssembly())
+        {
+            AssetDatabase.SaveAssets();
+            string path = EditorUtility.OpenFolderPanel("Where to save?", "", "");
+            if (!string.IsNullOrEmpty(path))//In case they canceled the action
+            {
+                BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows64);
+                Debug.Log("Export was succesfull.");
+            }
+            ClearCache();
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Error", "No assembly found", "ok");
+        }
+     }
+
+    static bool SetAssembly()
+    {
+        string[] assets = AssetDatabase.FindAssets("t:GameInfoData");
+        if (assets != null && assets.Length > 0)
+        {
+            GameInfoData info = AssetDatabase.LoadMainAssetAtPath(AssetDatabase.GUIDToAssetPath(assets[0])) as GameInfoData;
+            if (info)
+            {
+                string assemblyDef = info.AssemblyDefinition;
+                DirectoryInfo dirInfo = new DirectoryInfo(Application.dataPath);
+                dirInfo = new DirectoryInfo(Path.Combine(dirInfo.Parent.ToString(), "Library\\ScriptAssemblies"));
+                string path = dirInfo + "\\" + assemblyDef + ".dll";
+
+                if (File.Exists(path))//Is there an assembly?
+                {
+                    string nameAssembly = "assembly.bytes";
+                    _pathAssembly = Path.Combine(Application.dataPath, nameAssembly);
+
+                    ClearCache();//In case the file already exist
+
+                    File.Copy(path, _pathAssembly);
+                    AssetDatabase.Refresh();
+                    info.AssemblyTextAsset = (TextAsset)AssetDatabase.LoadAssetAtPath("Assets/"+nameAssembly, typeof(TextAsset));
+                    if( info.AssemblyTextAsset != null)
+                    {
+                        return true;
+                    }
+                }
+                Debug.LogWarning("Assembly does not exist, or wrong name...");
+            }
+            Debug.LogWarning("Couldn't find you 'Game Info Data'");
+        }
+
+        return false;
+    }
+
+    static void ClearCache()
+    {
+        if (!string.IsNullOrEmpty(_pathAssembly))
+        {
+            if (File.Exists(_pathAssembly + ".meta"))
+            {
+                File.Delete(_pathAssembly + ".meta");
+            }
+            if (File.Exists(_pathAssembly))
+            {
+                File.Delete(_pathAssembly);
+            }
+
+            AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ Assets/Scripts/SlimeMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SlimeMovement : MonoBehaviour
+{
+    private Vector3 rawInputMovement;
+    private CharacterController _characterController;
+
+    [SerializeField]
+    private float _moveSpeed = 5;
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        Vector2 inputMovement = value.ReadValue<Vector2>();
+        rawInputMovement = new Vector3(inputMovement.x, 0, inputMovement.y);
+    }
+    public void OnAttack(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            Debug.Log("Attacking");
+        }
+    }
+    private void Movement()
+    {
+        _characterController.Move((rawInputMovement) * _moveSpeed * Time.deltaTime);
+    }
+    
+}
